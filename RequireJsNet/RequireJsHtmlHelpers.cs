76201d39ff944c8e840213e063514c6ffed8db36
Stefan Prodan <stefan.prodan@gmail.com>
2013-10-31 13:18:50 +0200

    extebd requireConfig js object to support locale

--- RequireJsNet/RequireJsHtmlHelpers.cs
+++ RequireJsNet/RequireJsHtmlHelpers.cs
@@ -47,10 +47,10 @@ namespace RequireJS
                 setupHtml.AppendLine("<script type=\"text/javascript\">");
 
                 setupHtml.AppendLine("var requireConfig = {");
-                setupHtml.AppendLine("locale:'" + html.CurrentCulture() + "'");
-                setupHtml.Append(",");
-                setupHtml.AppendLine("pageOptions:" + RequireJsOptions.ConvertToJsObject(html.ViewBag.PageOptions));
-                setupHtml.Append(",");
+                setupHtml.Append("locale:'" + html.CurrentCulture() + "'");
+                setupHtml.AppendLine(",");
+                setupHtml.Append("pageOptions:" + RequireJsOptions.ConvertToJsObject(html.ViewBag.PageOptions));
+                setupHtml.AppendLine(",");
                 setupHtml.AppendLine("websiteOptions:" + RequireJsOptions.ConvertToJsObject(html.ViewBag.GlobalOptions));
                 setupHtml.AppendLine("};");
 
@@ -93,10 +93,10 @@ namespace RequireJS
                 setupHtml.AppendLine("<script type=\"text/javascript\">");
 
                 setupHtml.AppendLine("var requireConfig = {");
-                setupHtml.AppendLine("locale:'" + html.CurrentCulture() + "'");
-                setupHtml.Append(",");
-                setupHtml.AppendLine("pageOptions:" + RequireJsOptions.ConvertToJsObject(html.ViewBag.PageOptions));
-                setupHtml.Append(",");
+                setupHtml.Append("locale:'" + html.CurrentCulture() + "'");
+                setupHtml.AppendLine(",");
+                setupHtml.Append("pageOptions:" + RequireJsOptions.ConvertToJsObject(html.ViewBag.PageOptions));
+                setupHtml.AppendLine(",");
                 setupHtml.AppendLine("websiteOptions:" + RequireJsOptions.ConvertToJsObject(html.ViewBag.GlobalOptions));
                 setupHtml.AppendLine("};");
 
