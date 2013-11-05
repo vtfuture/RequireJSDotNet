66f8e7b5c4af5b4c0100908657c50afd5037db60
Cezar Cretu <cretucezarandrei@gmail.com>
2013-11-05 15:56:57 +0200

    added original YUI.NET code to Compressor project

---
+++ RequireJsNet.Compressor/FileSpec.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequireJsNet.Compressor
+{
+    public class FileSpec
+    {
+        private string fileName;
+
+        public string FileName
+        {
+            get
+            {
+                return fileName;
+            }
+            set
+            {
+                fileName = value.Trim();
+            }
+        }
+
+        public string CompressionType { get; set; }
+
+        public FileSpec(string fileName, string compressionType)
+        {
+            FileName = fileName;
+            CompressionType = compressionType;
+        }
+    }
+}
---
+++ RequireJsNet.Compressor/ILog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequireJsNet.Compressor
+{
+    public interface ILog
+    {
+        void LogMessage(string message);
+
+        void LogBoolean(string name, bool value);
+
+        void LogError(string message, params object[] messageArgs);
+
+        void LogErrorFromException(Exception exception);
+
+        void LogErrorFromException(Exception exception, bool showStackTrace);
+    }
+}
