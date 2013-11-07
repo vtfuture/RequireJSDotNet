2209549f4de08f9b56c4dd943cd2c972d12b36c6
Cezar Cretu <cretucezarandrei@gmail.com>
2013-11-07 06:36:02 +0200

    bundles can now define their dependencies via the includes= attribute - resolved #3

--- RequireJsNet.Compressor/BundleDefinition.cs
+++ RequireJsNet.Compressor/BundleDefinition.cs
@@ -12,5 +12,7 @@ namespace RequireJsNet.Compressor
         public List<BundleItem> Items { get; set; }
         public bool IsVirtual { get; set; }
         public string OutputPath { get; set; }
+        public List<string> Includes { get; set; }
+        public bool ParsedIncludes { get; set; }
     }
 }
--- RequireJsNet.Compressor/RequireConfigReader.cs
+++ RequireJsNet.Compressor/RequireConfigReader.cs
@@ -42,6 +42,7 @@ namespace RequireJsNet.Compressor
             }
 
             ResolvePhysicalPaths();
+            ResolveBundleIncludes();
 
             var bundles = new List<Bundle>();
             foreach (var bundleDefinition in Configuration.Bundles.Where(r => !r.IsVirtual))
@@ -55,6 +56,87 @@ namespace RequireJsNet.Compressor
             }
 
             return bundles;
+        }
+
+
+        private void ResolveBundleIncludes()
+        {
+            var rootBundles = Configuration.Bundles.Where(r => !r.Includes.Any()).ToList();
+            if (!rootBundles.Any())
+            {
+                throw new Exception("Could not find any bundle with no dependency. Check your config for cyclic dependencies.");
+            }
+            rootBundles.ForEach(r => r.ParsedIncludes = true);
+            var maxIterations = 500;
+            var currentIt = 0;
+            while (Configuration.Bundles.Where(r => !r.ParsedIncludes).Any())
+            {
+                // shouldn't really happen, but we'll use this as a safeguard against an endless loop for the moment
+                if (currentIt > maxIterations)
+                {
+                    throw new Exception("Maximum number of iterations exceeded. Check your config for cyclick dependencies");
+                }
+
+                // get all the bundles that have parents with resolved dependencies and haven't been resolved themselves
+                var parsableBundles = GetBundlesWithResolvedParents();
+                // we've checked earlier if there are any bundles that haven't been parsed
+                // if there are bundles that haven't been parsed but there aren't any we can parse, something went wrong
+                if (!parsableBundles.Any())
+                {
+                    throw new Exception("Could not parse bundle includes. Check your config for cyclic dependencies.");
+                }
+                
+                foreach (var bundle in parsableBundles)
+                {
+                    // store a reference to the old list
+                    var oldItemList = bundle.Items;
+                    // instantiate a new one so that when we're done we can append the old scripts
+                    bundle.Items = new List<BundleItem>();
+                    var parents = bundle.Includes.Select(r => GetBundleByName(r)).ToList();
+                    foreach (var parent in parents)
+                    {
+                        bundle.Items.AddRange(parent.Items);
+                    }
+                    bundle.Items.AddRange(oldItemList);
+                    bundle.Items = bundle.Items.GroupBy(r => r.PhysicalPath).Select(r => r.FirstOrDefault()).ToList();
+                    bundle.ParsedIncludes = true;
+                }
+                currentIt++;
+            }
+
+        }
+
+        private BundleDefinition GetBundleByName(string name)
+        {
+            var result =  Configuration.Bundles.Where(r => r.Name == name).FirstOrDefault();
+            if (result == null)
+            {
+                throw new Exception("Could not find bundle with name " + name);
+            }
+            return result;
+        }
+
+        private List<BundleDefinition> GetBundlesWithResolvedParents()
+        {
+            var allBundles = Configuration.Bundles;
+            var result = new List<BundleDefinition>();
+            foreach (var bundle in allBundles.Where(r => !r.ParsedIncludes))
+            {
+                // for each include, get its bundle.ParsedIncludes property
+                // select those that don't have their parents resolved
+                // if any such items exist, it means that the item's parents haven't been resolved
+                var parentsResolved = !(bundle.Includes
+
+                                        .Select(r => GetBundleByName(r).ParsedIncludes)
+                        
+                        .Where(r => !r)
+                        .Any());
+                if (parentsResolved)
+                {
+                    result.Add(bundle);
+                }
+            }
+            return result;
         }
 
         private void ResolvePhysicalPaths()
@@ -160,6 +242,7 @@ namespace RequireJsNet.Compressor
                     Name = r.Attribute("name").Value,
                     IsVirtual = ReadBooleanAttribute(r.Attribute("virtual")),
                     OutputPath = ReadStringAttribute(r.Attribute("outputPath")),
+                    Includes = ReadStringListAttribute(r.Attribute("includes")),
                     Items = r.Descendants("bundleItem").Select(x => new BundleItem
                     {
                         ModuleName = x.Attribute("path").Value,
@@ -175,6 +258,16 @@ namespace RequireJsNet.Compressor
                     }
                 }
             }
+        }
+
+        private List<string> ReadStringListAttribute(XAttribute attribute)
+        {
+            if (attribute == null)
+            {
+                return new List<string>();
+            }
+            var result = attribute.Value.Split(',').Select(r => r.Trim()).Distinct().ToList();
+            return result;
         }
 
         private string ReadStringAttribute(XAttribute attribute)
