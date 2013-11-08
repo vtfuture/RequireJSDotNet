42bce8e5125513784a659a5303128c5334039f79
Cezar Cretu <cretucezarandrei@gmail.com>
2013-11-08 04:34:51 +0200

    added CssCompressorTask back

--- RequireJsNet.Compressor/ActionType.cs
+++ RequireJsNet.Compressor/ActionType.cs
@@ -3,6 +3,7 @@
     public enum ActionType
     {
         Unknown,
+        Css, 
         JavaScript
     }
 }
---
+++ RequireJsNet.Compressor/CssCompressorTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Yahoo.Yui.Compressor;
+
+namespace RequireJsNet.Compressor
+{
+    public class CssCompressorTask : CompressorTask
+    {
+        private readonly ICssCompressor _compressor;
+
+        public bool PreserveComments { get; set; }
+
+        public CssCompressorTask()
+            : this(new CssCompressor())
+        {
+        }
+
+        public CssCompressorTask(ICssCompressor compressor)
+            : base(compressor)
+        {
+            _compressor = compressor;
+        }
+
+        public override bool Execute()
+        {
+            _compressor.RemoveComments = !PreserveComments;
+            return base.Execute();
+        }
+    }
+}
