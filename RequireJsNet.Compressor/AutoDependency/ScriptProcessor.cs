fd429a45c66997fa37f1abaae01115733b4ffe63
Cezar Cretu <cretucezarandrei@gmail.com>
2014-05-16 04:50:51 +0300

    moved parsing/transformation code into its own folder, started working on ScriptProcessor

---
+++ RequireJsNet.Compressor/AutoDependency/ScriptProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequireJsNet.Compressor.AutoDependency
+{
+    public class ScriptProcessor
+    {
+        public ScriptProcessor(string relativeFileName, string scriptText)
+        {
+            
+        }
+
+        public string OriginalString { get; set; }
+
+        public string ProcessedString { get; set; }
+
+
+    }
+}
