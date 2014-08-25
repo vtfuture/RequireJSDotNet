85ce1cc15461cf1fc312072919b3ce9bca6f796c
Cezar Cretu <cretucezarandrei@gmail.com>
2014-08-25 16:27:08 +0300

    fixed spacing around some file headers

--- RequireJsNet/Models/RequirePath.cs
+++ RequireJsNet/Models/RequirePath.cs
@@ -4,6 +4,7 @@
 // Dual licensed under the MIT and GPL licenses:
 // http://www.opensource.org/licenses/mit-license.php
 // http://www.gnu.org/licenses/gpl.html
+
 namespace RequireJsNet.Models
 {
     public class RequirePath
