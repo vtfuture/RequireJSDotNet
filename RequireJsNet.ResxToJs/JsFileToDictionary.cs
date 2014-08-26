149a851875663e36f32e2261751d38ee96b1ba67
Cezar Cretu <cretucezarandrei@gmail.com>
2014-08-26 14:45:17 +0300

    added resxtoJs proj

---
+++ RequireJsNet.ResxToJs/JsFileToDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace RequireJsNet.ResxToJs
+{
+    internal class JsFileToDictionary
+    {
+        internal JsFileToDictionary(List<string> lines)
+        {
+            FileLines = lines;
+        }
+
+        private List<string> FileLines { get; set; }
+
+        internal Dictionary<string, string> GetDictionary()
+        {
+            CleanLines();
+            var builder = new StringBuilder();
+            FileLines.ForEach(line => builder.Append(line));
+            var deserialized = JsonConvert.DeserializeObject<Dictionary<string, string>>(builder.ToString());
+            return deserialized;
+        }
+
+        private void CleanLines()
+        {
+            var cleaned = new List<string>();
+            FileLines.ForEach(line =>
+            {
+                if (!IsSingleLineComment(line))
+                {
+                    cleaned.Add(RemoveMethodComponents(line));
+                }
+            });
+            FileLines = cleaned;
+        }
+
+
+        private bool IsSingleLineComment(string line)
+        {
+            return line.Trim().StartsWith("//");
+        }
+
+        private string RemoveMethodComponents(string line)
+        {
+            return line.Replace("define(", string.Empty).Replace("});", "}");
+        }
+    }
+}
---
+++ RequireJsNet.ResxToJs/ResxToJsTask.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+using Newtonsoft.Json;
+
+using RequireJsNet.ResxToJs.Models;
+
+namespace RequireJsNet.ResxToJs
+{
+    public class ResxToJsTask : Task
+    {
+        [Required]
+        public string RootResx { get; set; }
+        [Required]
+        public string RootJs { get; set; }
+
+        public override bool Execute()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            if (!CheckMainFilesExistance())
+            {
+                return false;
+            }
+
+            try
+            {
+                var languages = ReadJSFile(RootJs);
+                var cultures = GetAvailableCultures(languages.Where(r => r.Value.ToLower() == "true").Select(r => r.Key).ToList());
+                GetResources(cultures);
+                WriteJsFiles(cultures);
+            }
+            catch (Exception ex)
+            {
+                Log.LogMessage(MessageImportance.High, ex.Message);
+                return false;
+            }
+
+            sw.Stop();
+            Log.LogMessage(MessageImportance.High, "Resource files generated in " + sw.ElapsedMilliseconds + "ms.");
+            return true;
+        }
+
+        private void GetResources(List<ResourceInfo> resouceInfo)
+        {
+            var rootResInfo = resouceInfo.Where(r => r.CultureShort.ToLower() == "root").FirstOrDefault();
+            if (rootResInfo == null)
+            {
+                throw new Exception("Could not find root resources");
+            }
+
+            var xdoc = XDocument.Load(rootResInfo.ResxPath);
+            rootResInfo.Resources = xdoc.Descendants("data")
+                    .Where(x => x.Element("comment") != null && x.Element("comment").Value.Trim() == "js")
+                    .ToDictionary(x => x.Attribute("name").Value, y => y.Element("value").Value);
+            var keys = rootResInfo.Resources.Select(r => r.Key).ToList();
+
+            foreach (var resInfoItem in resouceInfo.Where(r => r.CultureShort.ToLower() != "root"))
+            {
+                if (!File.Exists(resInfoItem.ResxPath))
+                {
+                    throw new FileNotFoundException("Could not load file " + resInfoItem.ResxPath, resInfoItem.ResxPath);
+                }
+
+                var xdocLocalized = XDocument.Load(resInfoItem.ResxPath);
+                resInfoItem.Resources = xdocLocalized.Descendants("data")
+                        .Where(x => keys.Contains(x.Attribute("name").Value))
+                        .ToDictionary(x => x.Attribute("name").Value, y => y.Element("value").Value);
+            }
+        }
+
+        private bool CheckMainFilesExistance()
+        {
+            return CheckFileExistance(RootJs) && CheckFileExistance(RootResx);
+        }
+
+        private bool CheckFileExistance(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Log.LogMessage(MessageImportance.High, "Could not read file " + path);
+                return false;
+            }
+
+            return true;
+        }
+
+        private Dictionary<string, string> ReadJSFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Could not find file " + path, path);
+            }
+
+            var lines = File.ReadAllLines(path).ToList();
+            var fileToDict = new JsFileToDictionary(lines);
+            return fileToDict.GetDictionary();
+        }
+
+        private List<ResourceInfo> GetAvailableCultures(List<string> jsCultures)
+        {
+            var jsFileInfo = new FileInfo(RootJs); 
+            var rootJsPath = jsFileInfo.Directory.FullName;
+            var jsFilename = jsFileInfo.Name;
+
+            var rootResxPath = new FileInfo(RootResx).Directory.FullName;
+            var allResxFiles = Directory.GetFiles(rootResxPath, "*.resx");
+            var availableCultures = new List<ResourceInfo>();
+
+            foreach (string resxFile in allResxFiles)
+            {
+                var file = new FileInfo(resxFile).Name;
+                var match = Regex.Match(file, @"\.([a-z]+?)\-", RegexOptions.IgnoreCase);
+                var locale = "Root";
+                if (match.Success)
+                {
+                    locale = match.Groups[1].Value;
+                }
+
+                if (jsCultures.Where(r => r.ToLower() == locale.ToLower()).Any())
+                {
+                    availableCultures.Add(new ResourceInfo
+                    {
+                        CultureShort = locale,
+                        ResxPath = resxFile,
+                        ProjectedJsPath = Path.Combine(rootJsPath, locale, jsFilename)
+                    });
+                }
+            }
+
+            return availableCultures;
+        }
+
+        private void WriteJsFiles(IEnumerable<ResourceInfo> resInfo)
+        {
+            foreach (var resourceInfo in resInfo)
+            {
+                var sb = new StringBuilder();
+                sb.Append("// DO NOT EDIT" + Environment.NewLine);
+                sb.Append("// Autogenerated on " + DateTime.Now + Environment.NewLine);
+                sb.Append("// There are " + resourceInfo.Resources.Keys.Count + " keys" + Environment.NewLine);
+                sb.Append(GetDefineCall(resourceInfo.Resources));
+                var directory = Path.GetDirectoryName(resourceInfo.ProjectedJsPath);
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                if (File.Exists(resourceInfo.ProjectedJsPath))
+                {
+                    var fInfo = new FileInfo(resourceInfo.ProjectedJsPath);
+                    fInfo.IsReadOnly = false;
+                    fInfo.Refresh();
+                }
+
+                File.WriteAllText(resourceInfo.ProjectedJsPath, sb.ToString());
+            }
+        }
+
+        private string GetDefineCall(Dictionary<string, string> resources)
+        {
+            var sb = new StringBuilder();
+            sb.Append("define(");
+            var js = JsonConvert.SerializeObject(resources, Formatting.Indented);
+            sb.Append(js);
+            sb.Append(");");
+            return sb.ToString();
+        }
+    }
+}
