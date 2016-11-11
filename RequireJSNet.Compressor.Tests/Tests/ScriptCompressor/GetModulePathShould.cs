df138dd43dd34f8ad6ce946b468b1f6207a8f5a2
Mattias Åslund <mattias@trustfall.se>
2016-11-11 21:36:11 +0700

    Code cleanup

--- RequireJSNet.Compressor.Tests/Tests/ScriptCompressor/GetModulePathShould.cs
+++ RequireJSNet.Compressor.Tests/Tests/ScriptCompressor/GetModulePathShould.cs
@@ -2,19 +2,7 @@
 using RequireJsNet.Compressor.Tests.Helpers;
 using RequireJsNet.Configuration;
 using RequireJsNet.Models;
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-//using RequireJsNet.Configuration;
-//using RequireJsNet.Models;
-//using RequireJsNet.Tests.DataCreation;
-//using RequireJsNet.Tests.Extensions;
-//using RequireJsNet.Tests.TestImplementations;
-
 using Xunit;
 
 namespace RequireJSNet.Compressor.Tests
