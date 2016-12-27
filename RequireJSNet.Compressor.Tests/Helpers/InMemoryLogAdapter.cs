6ce3ea3b463344cc79226993d3cf2410e7dd07b4
Mattias Åslund <mattias@trustfall.se>
2016-12-27 11:50:08 +0700

    Making sure each compressed file in a bundle starts on its own line  - otherwise its define() can become commented out by a previous //# sourceMappingURL comment

---
+++ RequireJSNet.Compressor.Tests/Helpers/InMemoryLogAdapter.cs
@@ -0,0 +1,72 @@
+﻿using EcmaScript.NET;
+using RequireJsNet.Compressor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequireJSNet.Compressor.Tests.Helpers
+{
+    public class InMemoryLogAdapter : ILog
+    {
+        readonly StringBuilder logger;
+
+        public InMemoryLogAdapter()
+        {
+            logger = new StringBuilder();
+        }
+
+        public override string ToString()
+        {
+            return logger.ToString();
+        }
+
+        public void LogMessage(string message)
+        {
+            logger.AppendLine(message);
+        }
+
+        public void LogBoolean(string name, bool value)
+        {
+            LogMessage(name + ": " + (value ? "Yes" : "No"));
+        }
+
+        public void LogError(string message, params object[] messageArgs)
+        {
+            logger.AppendFormat("ERROR: " + message, messageArgs);
+        }
+
+        public void LogErrorFromException(Exception exception)
+        {
+            this.logger.AppendFormat("EXCEPTION: {0}", exception.Message);
+        }
+
+        public void LogErrorFromException(Exception exception, bool showStackTrace)
+        {
+            this.logger.AppendFormat("EXCEPTION: {0}", showStackTrace ? exception.ToString() : exception.Message);
+        }
+
+        public void LogEcmaError(EcmaScriptException ecmaScriptException)
+        {
+            this.LogError("An error occurred in parsing the Javascript file.");
+            if (ecmaScriptException.LineNumber == -1)
+            {
+                this.LogError("[ERROR] {0} ********", ecmaScriptException.Message);
+            }
+            else
+            {
+                var sourceName = string.IsNullOrEmpty(ecmaScriptException.SourceName)
+                                     ? string.Empty
+                                     : "Source: {1}. " + ecmaScriptException.SourceName;
+                this.LogError(
+                    "[ERROR] {0} ******** Line: {2}. LineOffset: {3}. LineSource: \"{4}\"",
+                    ecmaScriptException.Message,
+                    sourceName,
+                    ecmaScriptException.LineNumber,
+                    ecmaScriptException.ColumnNumber,
+                    ecmaScriptException.LineSource);
+            }
+        }
+    }
+}
---
+++ RequireJSNet.Compressor.Tests/Tests/CompressorTaskEngine/ExecuteShould.cs
@@ -0,0 +1,68 @@
+﻿using RequireJsNet.Compressor.AutoDependency;
+using RequireJsNet.Compressor.Tests.Helpers;
+using RequireJsNet.Configuration;
+using RequireJsNet.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace RequireJSNet.Compressor.Tests.Tests.CompressorTaskEngine
+{
+    public class ExecuteShould
+    {
+        readonly string projectPath;
+
+        public ExecuteShould()
+        {
+            var executingDirectory = Directory.GetCurrentDirectory();
+            var directory = Path.Combine(executingDirectory, @"..\..\TestData\ExecuteShould");
+            projectPath = Path.GetFullPath(directory);
+        }
+
+        [Fact]
+        public void BundleFilesOnSeparateRows()
+        {
+            //If a bundled file ends with a //# sourceMappingURL comment, the next define() would be commented out
+            var yui = new Yahoo.Yui.Compressor.JavaScriptCompressor { Encoding = System.Text.Encoding.UTF8 };
+            var taskLog = new Helpers.InMemoryLogAdapter();
+
+            var files = new[] {
+                new RequireJsNet.Compressor.FileSpec(projectPath + @"\Scripts\BundleIncludedDirectory\a.js", null)
+                {
+                    FileContent ="define('bundleincludeddirectory/a', [],function () {\r\n    console.log('a.js');\r\n});"
+                },
+                new RequireJsNet.Compressor.FileSpec(projectPath + @"\Scripts\BundleIncludedDirectory\b.js", null)
+                {
+                    FileContent ="define('bundleincludeddirectory/b', [],function () {\r\n    console.log('b.js');\r\n});\r\n"
+                }
+            };
+
+            var expected = "define(\"bundleincludeddirectory/a\",[],function(){console.log(\"a.js\")});\r\ndefine(\"bundleincludeddirectory/b\",[],function(){console.log(\"b.js\")});\r\n";
+
+            var outputFile = Path.GetTempFileName();
+            try
+            {
+                var taskEngine = new RequireJsNet.Compressor.CompressorTaskEngine(taskLog, yui)
+                {
+                    CompressionType = "standard",
+                    DeleteSourceFiles = false,
+                    LineBreakPosition = -1,
+                    LoggingType = "none",
+                    OutputFile = outputFile,
+                    SourceFiles = files
+                };
+                taskEngine.Execute();
+
+                var actual = File.ReadAllText(outputFile);
+
+                Assert.Equal(expected, actual);
+            }
+            finally
+            {
+                File.Delete(outputFile);
+            }
+        }
+    }
+}
